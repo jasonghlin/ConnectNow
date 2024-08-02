0f16b17dbd5d1a8a84ee55f325ee8dab815baee9
GHL <devtest0204@gmail.com>
2024-08-02 16:26:23 +0800

    fix peer-server-4

--- practice/peer.js
+++ practice/peer.js
@@ -1,42 +1,23 @@
 import { PeerServer } from "peer";
 import dotenv from "dotenv";
-import cors from "cors";
-import express from "express";
-
 dotenv.config();
 
 const { ENV } = process.env;
-const app = express();
-
-// 配置CORS中间件
-app.use(
-  cors({
-    origin: "https://www.connectnow.website", // 允许的源，可以根据需要修改
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-
-let peerServer;
-let port;
 
 if (ENV === "production") {
-  port = 9001;
+  PeerServer({
+    port: 9001,
+    path: "/myapp",
+    // proxied: false,
+    proxied: true,
+    secure: true,
+  });
 } else {
-  port = 9001;
+  PeerServer({
+    port: 9001,
+    path: "/myapp",
+    // proxied: false,
+    proxied: true,
+    secure: true,
+  });
 }
-
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-peerServer = PeerServer({
-  path: "/myapp",
-  proxied: true,
-  secure: ENV === "production", // 根据环境设置secure属性
-});
-
-app.use("/peerjs", peerServer);
-app.get("/", (req, res) => {
-  res.send("PeerJS server is running");
-});
