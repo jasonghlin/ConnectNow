a129cf081eeab42aa1ce6bc5bc5a3721336f584d
GHL <devtest0204@gmail.com>
2024-08-30 21:08:33 +0800

    change back google auth redirect url

--- practice/public/js/routerRoomLoginRegister.js
+++ practice/public/js/routerRoomLoginRegister.js
@@ -166,5 +166,5 @@ handleLogin();
 // handle logout
 
 document.querySelector(".google-login").addEventListener("click", (e) => {
-  window.location.href = "https://www.connectnow.website/auth/google";
+  window.location.href = "/auth/google";
 });
