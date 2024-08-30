23139344eb5247a3038da01b9dc7384e5bec3953
GHL <devtest0204@gmail.com>
2024-08-30 21:05:11 +0800

    fix google auth redirect url

--- practice/public/js/routerRoomLoginRegister.js
+++ practice/public/js/routerRoomLoginRegister.js
@@ -166,5 +166,5 @@ handleLogin();
 // handle logout
 
 document.querySelector(".google-login").addEventListener("click", (e) => {
-  window.location.href = window.location.origin + "/auth/google";
+  window.location.href = "https://www.connectnow.website/auth/google";
 });
