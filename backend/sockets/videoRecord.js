933a088301361d241b244ae45817f6d0071da6fb
GHL <devtest0204@gmail.com>
2024-10-09 10:00:42 +0800

    add video record server log

--- backend/sockets/videoRecord.js
+++ backend/sockets/videoRecord.js
@@ -1,11 +1,11 @@
 export default function socketVideoRecord(io, socket) {
   socket.on("start-recording", (roomName) => {
-    console.log("received start-recording");
+    console.log(`Ｒeceived start-recording event for room: ${roomName}`);
     io.to(roomName).emit("start-recording");
   });
 
   socket.on("stop-recording", (roomName) => {
-    console.log("received stop-recording");
+    console.log(`Ｒeceived stop-recording event for room: ${roomName}`);
     io.to(roomName).emit("stop-recording");
   });
 }
